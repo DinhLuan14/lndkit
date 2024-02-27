9c21f5921f8568043216dcec8f31906dbf4349ea
luanngo <luanngo@Luannd-Macbook.local>
2024-02-27 23:45:07 +0700

    remove package pyvi

--- build/lib/lndkit/__init__.py
+++
@@ -1,9 +0,0 @@
-from .json_utils import read_json, write_json
-from .speedy import cachef
-from .utils import (
-    dump_json_or_pickle,
-    hashf,
-    load_json_or_pickle,
-    mkdir_or_exist,
-    timef,
-)
